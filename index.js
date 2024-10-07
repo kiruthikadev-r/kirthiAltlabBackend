6b5515f6ed089d15a41c7971d62f3ddbffa7be61
kiruthi <priyakiruthi21@gmail.com>
2024-10-08 02:01:57 +0530

    added unique id

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const corsOptions = {
   origin: 'https://kirthialtlabfrontend.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'], 
+  allowedHeaders: ['Content-Type', 'Authorization', 'uniqueId'], 
   credentials: true, 
 };
 
